6e0d1d3018d08a91877dba3392b59642228d113b
Clement <clementmarteau426@gmail.com>
2025-03-06 06:48:34 +0100

    Ajout des activités choix_montant et historique

---
+++ app/src/main/java/iut/dam/sae_app_mobile_france_asso/SecondFragment.java
@@ -0,0 +1,20 @@
+package iut.dam.sae_app_mobile_france_asso;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SecondFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+}
