0c01171fbcf8f26a86ddd8e23a1c906d07cd5eb3
Clement <clementmarteau426@gmail.com>
2025-03-31 20:59:47 +0200

    Ajout de l'historique des dons

--- app/src/main/java/iut/dam/sae_app_mobile_france_asso/HistoriqueActivity.java
+++ app/src/main/java/iut/dam/sae_app_mobile_france_asso/HistoriqueActivity.java
@@ -1,8 +1,18 @@
 package iut.dam.sae_app_mobile_france_asso;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,12 +20,29 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class HistoriqueActivity extends AppCompatActivity {
-    private TabLayout tabLayout;
-    private ViewPager2 viewPager;
+    private String adminAssoID;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private List<DonRecurrent> donsRecurrents;
+    private HistDonRecurrentAdapter adapter;
+    private Map<String, String> users;
+    private Map<String, String> donsUniques;
+    private ListView histDonsList;
+    private Spinner spinnerAnnee;
+    private TextView montantTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +50,141 @@ public class HistoriqueActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_historique);
 
-        tabLayout = findViewById(R.id.lab_layout);
-        viewPager = findViewById(R.id.view_pager);
+        adminAssoID = getIntent().getStringExtra("idAsso");
 
-        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), getLifecycle());
-        vpAdapter.addFragment(new HistRecurrentFragment(), "Dons récurrents");
-        vpAdapter.addFragment(new HistUniqueFragment(), "Dons uniques");
+        donsRecurrents = new ArrayList<>();
+        users = new HashMap<>();
+        donsUniques = new HashMap<>();
 
-        viewPager.setAdapter(vpAdapter);
+        histDonsList = findViewById(R.id.hist_dons_list);
+        adapter = new HistDonRecurrentAdapter(this, R.layout.item_hist_don, donsRecurrents);
+        histDonsList.setAdapter(adapter);
 
-        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
-            tab.setText(vpAdapter.getTitle(position));
-        }).attach();
+        spinnerAnnee = findViewById(R.id.spinner_annee);
+        montantTotal = findViewById(R.id.text_montant_total);
+
+        fetchAllDonsUniquesASync();
+        fetchAllDonsRecurrentsAndUsersASync();
+
+        ImageButton btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(v -> finish());
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                finish();
+            }
+        });
+    }
+
+
+
+    public void fetchAllDonsRecurrentsAndUsersASync(){
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            users.clear();
+                            for (QueryDocumentSnapshot document : task.getResult()){
+                                String userId = document.getId();
+                                String firstName = (String) document.getData().get("firstname");
+                                String lastName = (String) document.getData().get("name");
+
+                                users.put(userId, lastName.toUpperCase() + " " + firstName.substring(0, 1).toUpperCase() + firstName.substring(1).toLowerCase());
+                            }
+                            fetchAllDonsRecurrentsASync();
+                        }else {
+                            Log.d(this.getClass().getName(), "error");
+                        }
+                    }
+                });
+    }
+
+    public void fetchAllDonsRecurrentsASync(){
+        db.collection("dons_recurrents")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            donsRecurrents.clear();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String associationId = getFirstKeyFromDocumentId(document.getId());
+                                if (associationId.equals(adminAssoID)) {
+                                    String userName = users.get(getSecondKeyFromDocumentId(document.getId()));
+                                    String montant = String.valueOf(document.getData().get("montant"));
+                                    String periodicite = (String) document.getData().get("periodicite");
+
+                                    DonRecurrent d = new DonRecurrent(associationId, userName, montant, periodicite);
+
+                                    donsRecurrents.add(d);
+                                }
+                            }
+                            adapter.notifyDataSetChanged();
+                        }else {
+                            Log.d(this.getClass().getName(), "error");
+                        }
+                    }
+                });
+    }
+
+    public void fetchAllDonsUniquesASync(){
+        db.collection("dons_uniques")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            donsUniques.clear();
+                            for (QueryDocumentSnapshot document : task.getResult()){
+                                String annee = getFirstKeyFromDocumentId(document.getId());
+                                String associationId = getSecondKeyFromDocumentId(document.getId());
+                                String montant = String.valueOf(document.getData().get("montantTotal"));
+
+                                if (associationId.equals(adminAssoID))
+                                    donsUniques.put(annee, montant+" €");
+                            }
+                            setDonsUniques();
+                        }else {
+                            Log.d(this.getClass().getName(), "error");
+                        }
+                    }
+                });
+    }
+
+    public String getFirstKeyFromDocumentId(String documentId) {
+        if (documentId.contains("_")) {
+            return documentId.split("_")[0]; // Prend la partie avant "_"
+        }
+        return null; // Retourne null si le format est incorrect
+    }
+
+    public String getSecondKeyFromDocumentId(String documentId) {
+        if (documentId.contains("_")) {
+            return documentId.split("_")[1]; // Prend la partie après "_"
+        }
+        return null; // Retourne null si le format est incorrect
+    }
+
+    public void setDonsUniques(){
+        List<String> annees = new ArrayList<>(donsUniques.keySet());
+
+        ArrayAdapter<String> adapterSpinner = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, annees);
+        adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerAnnee.setAdapter(adapterSpinner);
+
+        spinnerAnnee.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedYear = annees.get(position);
+                montantTotal.setText(donsUniques.get(selectedYear));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                montantTotal.setText("");
+            }
+        });
     }
 }
--- app/src/main/java/iut/dam/sae_app_mobile_france_asso/ProfileActivity.java
+++ app/src/main/java/iut/dam/sae_app_mobile_france_asso/ProfileActivity.java
@@ -3,6 +3,7 @@ package iut.dam.sae_app_mobile_france_asso;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,28 +11,73 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import android.widget.ImageButton;
 import androidx.activity.OnBackPressedCallback;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FirebaseAuth mAuth;
     private TextView tvEmail, tvUserId;
+    private boolean isAdmin;
+    private Bundle bundle;
+    private Button buttonHist;
+    private TextView associationName;
+    private TextView associationText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        bundle = new Bundle();
+
+        buttonHist = findViewById(R.id.btnHist);
+
         mAuth = FirebaseAuth.getInstance();
-        tvEmail = findViewById(R.id.tvEmail);
-        tvUserId = findViewById(R.id.tvUserId);
+        tvEmail = findViewById(R.id.profile_mail);
+        tvUserId = findViewById(R.id.profile_id);
+
+        associationText = findViewById(R.id.profile_asso_text);
+        associationName = findViewById(R.id.profile_asso);
 
         FirebaseUser user = mAuth.getCurrentUser();
         if (user != null) {
-            tvEmail.setText("Email: " + user.getEmail());
-            tvUserId.setText("User ID: " + user.getUid());
+            tvEmail.setText(user.getEmail());
+            tvUserId.setText(user.getUid());
+
+            DocumentReference userRef = db.collection("users").document(user.getUid());
+            userRef.get().addOnSuccessListener(documentSnapshot -> {
+                if (documentSnapshot.exists()) {
+                    isAdmin = Boolean.TRUE.equals(documentSnapshot.getBoolean("isAdmin"));
+                    if (isAdmin) {
+                        String idAsso = documentSnapshot.getString("idAsso");
+                        bundle.putString("idAsso", idAsso);
+
+                        buttonHist.setVisibility(View.VISIBLE);
+                        associationName.setVisibility(View.VISIBLE);
+                        associationText.setVisibility(View.VISIBLE);
+
+                        if (idAsso != null && !idAsso.isEmpty()) {
+                            DocumentReference assoRef = db.collection("associations").document(idAsso);
+                            assoRef.get().addOnSuccessListener(assoSnapshot -> {
+                                if (assoSnapshot.exists()) {
+                                    String assoName = assoSnapshot.getString("name");
+                                    if (assoName != null) {
+                                        associationName.setText(assoName);
+                                    }
+                                }
+                            }).addOnFailureListener(e ->
+                                    Toast.makeText(this, "Erreur de récupération de l'association", Toast.LENGTH_SHORT).show()
+                            );
+                        }
+                    }
+                }
+            }).addOnFailureListener(e -> {
+                Toast.makeText(this, "Erreur de récupération des données", Toast.LENGTH_SHORT).show();
+            });
         } else {
             Toast.makeText(this, "Aucun utilisateur connecté", Toast.LENGTH_SHORT).show();
             finish();
@@ -52,5 +98,11 @@ public class ProfileActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public void clickHistDon(View view){
+        Intent intent = new Intent(this, HistoriqueActivity.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
     }
 }
